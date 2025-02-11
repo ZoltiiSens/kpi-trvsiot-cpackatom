8d57c4c2ce735a0ff28a552875e417bd58cd44ad
Napchik <91258817+Napchik@users.noreply.github.com>
2025-02-11 15:18:25 +0200

    adding new sensor for parking

--- src/file_datasource.py
+++ src/file_datasource.py
@@ -3,20 +3,25 @@ from datetime import datetime
 from domain.aggregated_data import AggregatedData
 from domain.accelerometer import Accelerometer
 from domain.gps import Gps
+from domain.parking import Parking
+import config
 
 
 class FileDatasource:
-    def __init__(self, accelerometer_filename: str, gps_filename: str) -> None:
+    def __init__(self, accelerometer_filename: str, gps_filename: str, parking_filename: str) -> None:
         # filenames
         self.accelerometer_filename = accelerometer_filename
         self.gps_filename = gps_filename
+        self.parking_filename = parking_filename
 
         # data
         self.accelerometer_data = None
         self.gps_data = None
+        self.parking_data = None
 
         self.accelerometer_index = 0
         self.gps_index = 0
+        self.parking_index = 0
 
     def read(self) -> AggregatedData:
         """Метод повертає дані отримані з датчиків"""
@@ -25,13 +30,17 @@ class FileDatasource:
             self.accelerometer_index = 0
         if self.gps_index >= len(self.gps_data) - 1:
             self.gps_index = 0
+        if self.parking_index >= len(self.parking_data) - 1:
+            self.parking_index = 0
 
         self.accelerometer_index += 1
         self.gps_index += 1
+        self.parking_index += 1
 
         accelerometer_data = Accelerometer(*self.accelerometer_data[self.accelerometer_index])
         gps_data = Gps(*self.gps_data[self.gps_index])
-        return AggregatedData(accelerometer_data, gps_data, datetime.now())
+        parking_data = Parking(*self.parking_data[self.parking_index], Gps(*config.PARKING_SPOT))
+        return AggregatedData(accelerometer_data, gps_data, parking_data, datetime.now())
 
     def startReading(self, *args, **kwargs):
         """Метод повинен викликатись перед початком читання даних"""
@@ -39,6 +48,8 @@ class FileDatasource:
             self.accelerometer_data = list(reader(csvfile))
         with open(self.gps_filename, 'r') as csvfile:
             self.gps_data = list(reader(csvfile))
+        with open(self.parking_filename, 'r') as csvfile:
+            self.parking_data = list(reader(csvfile))
 
     def stopReading(self, *args, **kwargs):
         """Метод повинен викликатись для закінчення читання даних"""
